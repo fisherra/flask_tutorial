0f3c15e4c69619b4ebcb18e3a553b1c75f8e5184
Fisher Ankney <fisher.ankney@capspire.com>
2020-06-14 15:58:53 -0600

    create login form validation

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
+from flask import render_template, flash, redirect
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+
 
 @app.route('/')  # decorator modifies function that follows it
-@app.route('/index') # use decorator to register function as 'callback'
-def index(): # these decorators create an association with url and the index function
+@app.route('/index')  # use decorator to register function as 'callback'
+def index():  # these decorators create an association with url and the index function
     user = {'username': 'Fisher'}
     posts = [
         {
@@ -16,3 +18,13 @@ def index(): # these decorators create an association with url and the index fun
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
