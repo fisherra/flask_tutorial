c7670d552469e5e9934dcb2c566563d2ff7ca601
Fisher Ankney <fisher.ankney@capspire.com>
2020-06-15 12:50:19 -0600

    logout error fixed

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from app import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,9 +20,11 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
