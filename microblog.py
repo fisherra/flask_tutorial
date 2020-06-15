86b5823988a5b7063d05385e7f7f8dcd9bf295d3
Fisher Ankney <fisher.ankney@capspire.com>
2020-06-15 12:48:05 -0600

    logout errors

--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# for flask shell command
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
